886cc64262d6c04aa8bffbf7d086aa4d29c4afca
MrChampion2020 <kingedendgreat2017@gmail.com>
2024-12-27 16:05:17 +0100

    merge remote change

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -128,6 +128,76 @@
 
 
 
+// const express = require("express");
+// const router = express.Router();
+// const { validatePassword, checkPassword } = require("../middlewares/userMiddleware/userMiddlewares");
+// const { authUserProtect } = require("../middlewares/userMiddleware/authUsersMiddleware");
+// const { authAdminProtect } = require("../middlewares/adminMiddlewares/authAdminsMiddleware");
+// const { getUsers, getOneUser, createUser, updateUser, deleteUser, userLogin, updateUserStatus, notificationUpdate } = require("../controllers/usersControllers");
+// const { checkUserStatus } = require("../middlewares/userMiddleware/checkUserStatus");
+
+// // Import the OTP functions
+// const { sendOTPEmail, verifyOTP } = require("../utils/sendEmail");
+
+// // Route to send OTP email (triggered during login process)
+// router.route("/send-otp").post(async (req, res) => {
+//   const { email } = req.body;
+//   try {
+//     if (!email) {
+//       return res.status(400).send("Email is required.");
+//     }
+//     await sendOTPEmail(email);
+//     return res.status(200).send("OTP sent to your email. Please verify it to complete login.");
+//   } catch (error) {
+//     return res.status(500).send("Error sending OTP: " + error.message);
+//   }
+// });
+
+// // Route to verify OTP (to be called after the user enters the OTP)
+// router.route("/verify-otp").post((req, res) => {
+//   const { email, otp } = req.body;
+
+//   if (!email || !otp) {
+//     return res.status(400).send("Email and OTP are required.");
+//   }
+
+//   const isVerified = verifyOTP(email, otp);
+
+//   if (isVerified) {
+//     return res.status(200).send("OTP verified successfully.");
+//   } else {
+//     return res.status(400).send("Invalid or expired OTP.");
+//   }
+// });
+
+// router
+//   .route("/")
+//   .get(authAdminProtect, getUsers)
+//   .post(validatePassword, createUser);
+
+// router
+//   .route("/:id")
+//   .get(authUserProtect, getOneUser)
+//   .put(
+//     authUserProtect,
+//     checkUserStatus,
+//     checkPassword,
+//     validatePassword,
+//     updateUser
+//   )
+//   .delete(authAdminProtect, deleteUser);
+
+// router.route("/login").post(userLogin);
+
+// router.route("/:id/updatestatus").put(authAdminProtect, updateUserStatus);
+
+// router
+//   .route("/notifications/:id")
+//   .put(authUserProtect, checkUserStatus, notificationUpdate);
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
 const { validatePassword, checkPassword } = require("../middlewares/userMiddleware/userMiddlewares");
@@ -144,12 +214,13 @@ router.route("/send-otp").post(async (req, res) => {
   const { email } = req.body;
   try {
     if (!email) {
-      return res.status(400).send("Email is required.");
+      return res.status(400).json({ message: "Email is required." });
     }
     await sendOTPEmail(email);
-    return res.status(200).send("OTP sent to your email. Please verify it to complete login.");
+    return res.status(200).json({ message: "OTP sent to your email. Please verify it to complete login." });
   } catch (error) {
-    return res.status(500).send("Error sending OTP: " + error.message);
+    console.error("Error in /send-otp route:", error);
+    return res.status(500).json({ message: "Error sending OTP. Please try again later." });
   }
 });
 
@@ -196,3 +267,4 @@ router
   .put(authUserProtect, checkUserStatus, notificationUpdate);
 
 module.exports = router;
+
